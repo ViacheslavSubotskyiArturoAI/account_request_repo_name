d2763c4972ab0f9b8d2ef85e6501ef7066f31357
vsubotskyi <vsubotskyi@arturo.ai>
2024-09-06 16:06:41 +0300

    Trigger

--- terraform/account-sandbox.tf
+++ terraform/account-sandbox.tf
@@ -1,28 +1,28 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "vsubotskyi-aft-poc-sandbox@arturo.ai"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "AFT-PoC"
-    SSOUserEmail              = "viacheslav.subotskyi@intellias.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
+#module "sandbox" {
+#  source = "./modules/aft-account-request"
+#
+#  control_tower_parameters = {
+#    AccountEmail              = "vsubotskyi-aft-poc-sandbox@arturo.ai"
+#    AccountName               = "sandbox-aft"
+#    ManagedOrganizationalUnit = "AFT-PoC"
+#    SSOUserEmail              = "viacheslav.subotskyi@intellias.com"
+#    SSOUserFirstName          = "Sandbox"
+#    SSOUserLastName           = "AFT"
+#  }
+#
+#  account_tags = {
+#    "Learn Tutorial" = "AFT"
+#  }
+#
+#  change_management_parameters = {
+#    change_requested_by = "HashiCorp Learn"
+#    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#  }
+#
+#  custom_fields = {
+#    group = "non-prod"
+#  }
+#
+#  account_customizations_name = "sandbox"
+#}
 
